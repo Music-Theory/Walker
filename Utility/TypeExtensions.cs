925563366115945920e0a915d0a2e8dcad05affb
Ash Walker <ashurstwalker@gmail.com>
2017-09-26 11:56:04 -0400

    Working on various things

---
+++ Utility/TypeExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Walker.Utility {
+	using System;
+
+	public static class TypeExtensions {
+
+		public static Type GetRoot(this Type t) {
+			if (t == null) { throw new ArgumentNullException(); }
+			Type bType = t;
+			while (bType.BaseType != typeof(object)) {
+				bType = bType.BaseType;
+			}
+			return bType;
+		}
+
+	}
+}

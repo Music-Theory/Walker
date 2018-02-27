e9aa1883a314d447e9e4b864adeeeb6a247d5181
Ash Walker <ashurstwalker@gmail.com>
2018-02-27 16:38:36 -0500

    Reformated Matrix4F to use a 1-dimensional array for storage

--- Walker/Data/Geometry/Speed/Rotation/Matrix4F.cs
+++ Walker/Data/Geometry/Speed/Rotation/Matrix4F.cs
@@ -1,7 +1,11 @@
 ﻿namespace Walker.Data.Geometry.Speed.Rotation {
     using System;
+    using System.Linq;
     using Space;
 
+    /// <summary>
+    /// Row-major 4x4 matrix, stored in a float[16].
+    /// </summary>
     public struct Matrix4F {
         public static readonly Matrix4F Identity = new Matrix4F(1, 0, 0, 0,
             0, 1, 0, 0,
@@ -16,14 +20,14 @@
         // If you want to make the calculations lazy, use a bool[4,4]
         // and stuff like HasChanged, RowChanged(int), ColumnChanged(int)
 
-        float[,] values;
+        float[] values;
+
+        public float[] Values => values;
 
         public float this[int i, int j] {
-            get => values[i, j];
-            set => values[i, j] = value;
+            get => values[i + j * 4];
+            set => values[i + j * 4] = value;
         }
-
-        public float[,] Values => values;
 
         #region Rows & Columns
 
@@ -110,12 +114,12 @@
         #endregion
 
         public Matrix4F Transpose {
-            get => new Matrix4F(new[,] {
-                {this[0, 0], this[0, 1], this[0, 2], this[0, 3]},
-                {this[1, 0], this[1, 1], this[1, 2], this[1, 3]},
-                {this[2, 0], this[2, 1], this[2, 2], this[2, 3]},
-                {this[3, 0], this[3, 1], this[3, 2], this[3, 3]}
-            });
+            get => new Matrix4F(
+                this[0, 0], this[0, 1], this[0, 2], this[0, 3],
+                this[1, 0], this[1, 1], this[1, 2], this[1, 3],
+                this[2, 0], this[2, 1], this[2, 2], this[2, 3],
+                this[3, 0], this[3, 1], this[3, 2], this[3, 3]
+            );
             set => values = value.Transpose.Values;
         }
 
@@ -153,7 +157,11 @@
         public Matrix4F(float[,] values) {
             if (values.GetLength(0) != 4 || values.GetLength(1) != 4) { throw new ArgumentException("Must supply 4x4 array to 4x4 matrix constructor"); }
 
-            this.values = values;
+            this.values = new float[16];
+
+            for (int i = 0; i < values.GetLength(0); i++) {
+                for (int j = 0; j < values.GetLength(1); j++) { this[i, j] = values[i, j]; }
+            }
         }
 
         public Matrix4F(float[][] values) {
@@ -163,7 +171,7 @@
                 if (values[i].Length != 4) { throw new ArgumentException("Must supply 4x4 array to 4x4 matrix constructor"); }
             }
 
-            this.values = new float[4, 4];
+            this.values = new float[16];
             for (int i = 0; i < 4; i++) {
                 for (int j = 0; j < 4; j++) { this[i, j] = values[i][j]; }
             }
@@ -172,19 +180,11 @@
         public Matrix4F(params float[] values) {
             if (values.Length > 16) { throw new ArgumentException("Can't have more than 16 values in a 4x4 matrix"); }
 
-            this.values = new float[4, 4];
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
-                    int index = j * 4 + i;
-                    if (index >= values.Length) { return; }
-
-                    this[i, j] = values[j * 4 + i];
-                }
-            }
+            this.values = values;
         }
 
         public Matrix4F(Vector4F g1, Vector4F g2, Vector4F g3, Vector4F g4, bool columns = false) {
-            values = new float[4, 4];
+            values = new float[16];
             if (columns) {
                 C1 = g1;
                 C2 = g2;
@@ -198,12 +198,11 @@
             }
         }
 
-        public Matrix4F(float val) => values = new[,] {
-            {val, val, val, val},
-            {val, val, val, val},
-            {val, val, val, val},
-            {val, val, val, val}
-        };
+        public Matrix4F(float val)
+            : this(val, val, val, val,
+                   val, val, val, val,
+                   val, val, val, val,
+                   val, val, val, val) { }
 
         public static Matrix4F operator -(Matrix4F a) => a * -1;
 
